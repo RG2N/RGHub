43e9cfdd93c03dd00d260abb3423ec6558530236
RG2N <64289295+RG2N@users.noreply.github.com>
2021-07-02 16:38:48 -0400

    Create kat!.lua

---
+++ scripts/kat!.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet(('https://pastebin.com/raw/uCSJY8we'), true)) ()
+    local message = Instance.new("Message", workspace)
+    message.Text  = "CTRL to lock on someone. Alt to enable ESP."
+        wait(6)
+        message:Destroy()
